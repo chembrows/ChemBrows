2164a304c28ac7a4a116bc634bcee1586a2336fd
JPFrancoia <jeanpatrick.francoia@gmail.com>
2015-09-01 13:52:43 +0200

    The quick search does not search for authors anymore.

--- hosts.py
+++ hosts.py
@@ -500,27 +500,17 @@ def getData(company, journal, entry, response=None):
         return None
 
 
-    # Use extra blank spaces to facilitate LIKE SQL query
-    topic_simple = " "
+    if abstract is not None:
+        topic_simple = " " + functions.simpleChar(BeautifulSoup(abstract, "lxml").text) + functions.simpleChar(title) + " "
+    else:
+        topic_simple = " " + functions.simpleChar(title) + " "
 
     if abstract is None or abstract == '':
         abstract = "Empty"
-    else:
-        # Use BeautifulSoup to remove the tags, simply keeo the text
-        topic_simple += functions.simpleChar(BeautifulSoup(abstract, "lxml").text)
-        topic_simple += " "
-
-    if author is None:
-        author = "Empty"
-    else:
-        topic_simple += functions.simpleChar(author)
-        topic_simple += " "
-
-    topic_simple += functions.simpleChar(title)
-    topic_simple += " "
-
     if graphical_abstract is None:
         graphical_abstract = "Empty"
+    if author is None:
+        author = "Empty"
 
 
     return title, date, author, abstract, graphical_abstract, url, topic_simple
