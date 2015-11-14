f2559c0d3e008e63a23f8e4d4c8d543933e4a02c
JPFrancoia <jeanpatrick.francoia@gmail.com>
2015-11-14 17:38:47 +0100

    Modification in the deployment script.

--- setup.py
+++ setup.py
@@ -34,10 +34,25 @@ my_data_files += get_all_files_in_dir('.{}config{}fields{}'.format(os.sep, os.se
 my_data_files += get_all_files_in_dir('.{}config{}styles{}'.format(os.sep, os.sep, os.sep))
 
 # Remove sensitive files
-my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}searches.ini'.format(os.sep, os.sep)]))
-my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}options.ini'.format(os.sep, os.sep)]))
-my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}options.ini_save'.format(os.sep, os.sep)]))
-my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}twitter_credentials'.format(os.sep, os.sep)]))
+try:
+    my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}searches.ini'.format(os.sep, os.sep)]))
+except ValueError:
+    pass
+
+try:
+    my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}options.ini'.format(os.sep, os.sep)]))
+except ValueError:
+    pass
+
+try:
+    my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}options.ini_save'.format(os.sep, os.sep)]))
+except ValueError:
+    pass
+
+try:
+    my_data_files.remove(('.{}config{}'.format(os.sep, os.sep), ['.{}config{}twitter_credentials'.format(os.sep, os.sep)]))
+except ValueError:
+    pass
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
