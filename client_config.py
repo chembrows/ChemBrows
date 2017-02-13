5eb4e162cb95783825fcfdd5b5fcf5e671fe5864
JPFrancoia <jeanpatrick.francoia@gmail.com>
2017-02-13 22:46:12 +0100

    Removing all files used by PyUpdater bc of removal of the auto-update feature.

--- client_config.py
+++
@@ -1,6 +0,0 @@
-class ClientConfig(object):
-    PUBLIC_KEY = 'xWkxdPtvoXrhgecEAcvegs6K2KDabE9qL6/jtWoPs5A'
-    APP_NAME = 'ChemBrows'
-    COMPANY_NAME = 'ChemBrows'
-    UPDATE_URLS = ['http://www.chembrows.com/downloads/']
-    MAX_DOWNLOAD_RETRIES = 3
